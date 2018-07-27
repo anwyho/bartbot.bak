b35fff3f15ed61eb6e8fba002a207baafeecfc5f
Anthony Ho <anwyho@gmail.com>
2018-07-27 11:28:30 -0700

    Reorganized workspace

---
+++ misc/gotest/goTest.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello, my dudes")
+	resp, err := http.Get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=rock&key=ZSBD-57UA-9TVT-DWE9&dir=n&json=y")
+	fmt.Println(err)
+	defer resp.Body.Close()
+
+}
+
+// func getDepTimes() (ts []int, ok bool) {
+
+// }
