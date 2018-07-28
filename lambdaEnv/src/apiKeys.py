681e588d1707fd7aacc830b0d97cae4eda73b20d
Anthony Ho <anwyho@gmail.com>
2018-07-28 15:08:49 -0700

    Added license information

--- lambdaEnv/src/apiKeys.py
+++ lambdaEnv/src/apiKeys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# This file is licensed under GNU GLPv3. 
+# Check out the LICENSE file in the root directory for more information. 
 
 PUBLIC = 'MW9S-E7SL-26DU-VV8V' 
 PERSONAL = 'ZSBD-57UA-9TVT-DWE9'
