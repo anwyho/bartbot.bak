1589a4e3bc12fd44ce7e111d1a43cf4cf33a8897
Anthony Ho <anwyho@gmail.com>
2018-08-14 14:58:52 -0700

    Shell script for updating aws lambda function

--- awsHandlerSync.sh
+++ awsHandlerSync.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# TODO: add check for correct pwd
+
 echo "adding handler to zip..."
 cd lambdaEnv/src/
 zip ../../handler.zip handler.py >/dev/null
@@ -11,6 +14,9 @@ zip -gr ../../../../handler.zip !(boto*) >/dev/null
 echo "copying zip to s3..."
 cd ../../../..
 aws s3 cp handler.zip s3://bartbot-bucket/handler.zip >/dev/null
+
+echo "updating pyProcessMessages on lambda..."
+aws lambda update-function-code --function-name pyProcessMessages --s3-bucket bartbot-bucket --s3-key handler.zip >/dev/null
 
 echo "done."
 
