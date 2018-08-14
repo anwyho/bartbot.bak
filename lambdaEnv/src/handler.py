a95b89ab39d475505823427442a48425f1a4c866
Anthony Ho <anwyho@gmail.com>
2018-08-14 14:59:22 -0700

    Integrated apiInfo.py

--- lambdaEnv/src/handler.py
+++ lambdaEnv/src/handler.py
@@ -1,22 +1,25 @@
 from __future__ import print_function
+
 import boto3
 import botocore
 import json
 import requests
 
-print('Loading function')
+from json import JSONDecoder
+
+import apiInfo
+
+VERIFY_TOKEN = "my_awesome_token";
+PAGE_ACCESS_TOKEN = apiInfo.FB_PAGE_ACCESS_KEY
 
 def processMessage(event, context):
-  #print("Received event: " + json.dumps(event, indent=2))
+  print("Received event: " + json.dumps(event, indent=2))
   print("value1 = " + event['key1'])
   print("value2 = " + event['key2'])
   print("value3 = " + event['key3'])
+
   return event['key1']  # Echo back the first key value
-  #raise Exception('Something went wrong')
-
-  print("hello owrld")
-
-  print(event.queryStringParameters)
+  raise Exception('Something went wrong')
 
   # GET request
   # TODO
