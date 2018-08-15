55a0ee152f7c8babf1e12d3c36f3008bd696596f
Anthony Ho <anwyho@gmail.com>
2018-08-14 18:27:07 -0700

    Test commit

--- lambdaEnv/src/handler.py
+++ lambdaEnv/src/handler.py
@@ -14,18 +14,17 @@ PAGE_ACCESS_TOKEN = apiInfo.FB_PAGE_ACCESS_KEY
 
 def processMessage(event, context):
   print("Received event: " + json.dumps(event, indent=2))
-  print("value1 = " + event['key1'])
-  print("value2 = " + event['key2'])
-  print("value3 = " + event['key3'])
-
-  return event['key1']  # Echo back the first key value
-  raise Exception('Something went wrong')
 
   # GET request
   # TODO
 
   # POST request
   # TODO
+
+  return event['key1']  # Echo back the first key value
+  raise Exception('Something went wrong')
+
+
 
 
 
