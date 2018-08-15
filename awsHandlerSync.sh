5d2d38560c502e0591bd143e489b1465364e1598
Anthony Ho <anwyho@gmail.com>
2018-08-14 17:38:48 -0700

    Updated aws shell script to handle verbosity

--- awsHandlerSync.sh
+++ awsHandlerSync.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
 # TODO: add check for correct pwd
+echo "are you in \"../bartbot/\"? (y/n)"
+read inBartbot
+[ ! "$inBartbot" = "y" ] && exit 1
 
-echo "adding handler to zip..."
+# file descriptor 6 to null
+exec 6>/dev/null
+
+# toggle verbosity
+if [ $# -ge 1 ] && [ "$1" = "--verbose" ] || [ $# -ge 1 ] && [ "$1" = "-v" ]
+then
+  exec 6>&1
+  echo "verbose mode."
+fi
+
+echo "adding handler to zip..." >&1
 cd lambdaEnv/src/
-zip ../../handler.zip handler.py >/dev/null
+zip ../../handler.zip handler.py >&6 2>&1
 
-echo "adding packages to zip..."
+echo "adding packages to zip..." >&1
 cd ../../lambdaEnv/lib/python3.6/site-packages/
 shopt -s extglob
-zip -gr ../../../../handler.zip !(boto*) >/dev/null
+zip -gr ../../../../handler.zip !(boto*) >&6 2>&1
 
-echo "copying zip to s3..."
+echo "copying zip to s3..." >&1
 cd ../../../..
-aws s3 cp handler.zip s3://bartbot-bucket/handler.zip >/dev/null
+aws s3 cp handler.zip s3://bartbot-bucket/handler.zip >&6 2>&1
 
-echo "updating pyProcessMessages on lambda..."
-aws lambda update-function-code --function-name pyProcessMessages --s3-bucket bartbot-bucket --s3-key handler.zip >/dev/null
+echo "updating pyProcessMessages on lambda..." >&1
+aws lambda update-function-code --function-name pyProcessMessages --s3-bucket bartbot-bucket --s3-key handler.zip >&6 2>&1
 
 echo "done."
 
