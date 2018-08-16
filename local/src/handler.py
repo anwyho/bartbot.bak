baada26f6554b95f269f91abe574c3b1a93f2735
Anthony Ho <anwyho@gmail.com>
2018-08-16 13:32:24 -0700

    No Zappa functionality

--- local/src/handler.py
+++ local/src/handler.py
@@ -7,38 +7,43 @@ import os
 import requests
 import json
 from sys import argv
-from flask import Flask, request
-from wit import Wit
+from flask import Flask, request, redirect, url_for
+# from wit import Wit
 
 app = Flask(__name__)
 
-WIT_TOKEN = os.environ.get('WIT_TOKEN')
+# WIT_TOKEN = os.environ.get('WIT_TOKEN')
 FB_PAGE_TOKEN = os.environ.get('FB_PAGE_TOKEN')
 FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
 
 FB_PAGE_ACCESS_TOKEN = "EAADFvGcu0k4BAEDPXrULwvkFDP6tqmwLTOJJPah6GO9OEndFggkXMosHmRzO4edZAZCtEuFv6TvcLDDQvr6eMeb2XDZAZBLKoZAOIZAVPzgsQcn96AZCj10Ek653lsiUerZCqlchqqtZBxLh1S71XmYdAr7vsgEGvxCUpbyBiGzUrYAZDZD"
 FB_VERIFY_TOKEN = "OF_MY_APPRECIATION"
-WIT_TOKEN = "SS3OBO2R4BNYYUBW6EBI4WR2ZJO4VIF3"
+# WIT_TOKEN = "SS3OBO2R4BNYYUBW6EBI4WR2ZJO4VIF3"
 
-client = Wit(access_token=WIT_TOKEN)
+@app.route("/")
+def main_handle():
+    # print("in main handle")
+    # return redirect(url_for('webhook'))
+    return "In main handle"
 
-# @app.route("/")
-# def main_handle():
-#     return "in main handle"
 
 @app.route("/webhook", methods=['POST', 'GET'])
 def handle_webhook():
     if request.method == 'GET':
         return webhook_challenge(request)
-    else:
+    elif request.method == 'POST':
         # return simple_post(request)
         return messenger_post(request)
+    else:
+        return "Unsupported HTTPS Verb."
 
 
-def simple_post(request):
-    pass
+# def simple_post(request):
+#     data = request.args
+#     if data['object'] == 'page':
+#         req = requests.post()
 
-    
+
 def webhook_challenge(request):
     """
     A webhook to return a challenge
@@ -54,11 +59,12 @@ def webhook_challenge(request):
     else:
         return 'Invalid Request or Verification Token'
 
+
 def messenger_post(request):
     """
     Handler for webhook (currently for postback and messages)
     """
-    data = request.args
+    data = request.json
     if data['object'] == 'page':
         for entry in data['entry']:
             # get all the messages
@@ -69,17 +75,22 @@ def messenger_post(request):
                 # Yay! We got a new message!
                 # We retrieve the Facebook user ID of the sender
                 fb_id = message['sender']['id']
+                if fb_id != '<PSID>':
+                    raise ValueError("fb_id: {}".format(fb_id))
                 # We retrieve the message content
                 text = message['message']['text']
-                # Let's forward the message to Wit /message
-                # and customize our response to the message in handle_message
-                response = client.message(msg=text, context={'session_id':fb_id}, verbose=True)
-                handle_message(response=response, fb_id=fb_id)
-                return 'Handled Message'
+
+                fb_message(fb_id,text)
+
+                # # Let's forward the message to Wit /message
+                # # and customize our response to the message in handle_message
+                # response = Wit(access_token=WIT_TOKEN).message(msg=text, context={'session_id':fb_id}, verbose=True)
+                # handle_message(response=response, fb_id=fb_id)
     else:
         # Returned another event
         return 'Received Different Event'
-    return 'Did not handle message...'
+    return Non
+
 
 def fb_message(sender_id, text):
     """
@@ -98,32 +109,32 @@ def fb_message(sender_id, text):
     return resp.content
 
 
-def first_entity_value(entities, entity):
-    """
-    Returns first entity value
-    """
-    if entity not in entities:
-        return None
-    val = entities[entity][0]['value']
-    if not val:
-        return None
-    return val['value'] if isinstance(val, dict) else val
+# def first_entity_value(entities, entity):
+#     """
+#     Returns first entity value
+#     """
+#     if entity not in entities:
+#         return None
+#     val = entities[entity][0]['value']
+#     if not val:
+#         return None
+#     return val['value'] if isinstance(val, dict) else val
 
 
-def handle_message(response, fb_id):
-    """
-    Customizes our response to the message and sends it
-    """
-    entities = response['entities']
-    # Checks if user's message is a greeting
-    # Otherwise we will just repeat what they sent us
-    greetings = first_entity_value(entities, 'greetings')
-    if greetings:
-        text = "hello!"
-    else:
-        text = "We've received your message: " + response['_text']
-    # send message
-    fb_message(fb_id, text)
+# def handle_message(response, fb_id):
+#     """
+#     Customizes our response to the message and sends it
+#     """
+#     entities = response['entities']
+#     # Checks if user's message is a greeting
+#     # Otherwise we will just repeat what they sent us
+#     greetings = first_entity_value(entities, 'greetings')
+#     if greetings:
+#         text = "hello!"
+#     else:
+#         text = "We've received your message: " + response['_text']
+#     # send message
+#     fb_message(fb_id, text)
 
 
 if __name__ == '__main__':
