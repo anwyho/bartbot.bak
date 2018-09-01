200fc71c03ae20ddac153a155a3d275b56f2763e
Anthony Ho <anwyho@gmail.com>
2018-08-31 17:00:40 -0700

    Improved code path

--- src/handler.py
+++ src/handler.py
@@ -7,18 +7,42 @@ import hashlib
 import hmac
 import json
 import os
-import requests
+import random
+import requests as req
 
 from flask import Flask, request, redirect, url_for
 from wit import Wit
 
+from . import challenge
+
 app = Flask(__name__)
 
+# Environment variables
 FB_PAGE_ACCESS = os.environ.get('FB_PAGE_ACCESS')
 FB_PAGE_ACCESS_2 = os.environ.get('FB_PAGE_ACCESS_2')
 FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
 WIT_TOKEN = os.environ.get('WIT_TOKEN')
-FB_MESSAGES_URL = 'https://graph.facebook.com/v2.6/me/messages?'
+
+# URLs
+GRAPH_API = 'https://graph.facebook.com/'
+MESSENGER_PLATFORM = GRAPH_API+'v2.6/me/'
+MESSAGES_URL = MESSENGER_PLATFORM+'messages?'+gen_auth_queries()
+
+
+def get_random_int(lower_bound=1, upper_bound=10):
+    return random.randomint(lower_bound, upper_bound)
+
+
+def gen_auth_queries():
+    """Calculates AUTH from SHA256"""
+    return 'access_token={token}&appsecret_proof={proof}'.format(
+        token=FB_PAGE_ACCESS,
+        proof=hmac.new(
+            FB_PAGE_ACCESS_2.encode('utf-8'),
+            msg=FB_PAGE_ACCESS.encode('utf-8'),
+            digestmod=hashlib.sha256)
+                .hexdigest())
+
 
 @app.route("/")
 def main_handle():
@@ -29,68 +53,52 @@ def main_handle():
 
 @app.route("/webhook", methods=['POST', 'GET'])
 def handle_webhook():
-
     # TODO: Verify SHA-1
-
     if request.method == 'GET':
-        return webhook_challenge(request)
+        return challenge.process_get(request)
     elif request.method == 'POST':
-        # return simple_post(request)
-        return messenger_post(request)
+        return process_messages_event(request)
     else:
         return "Unsupported HTTPS Verb."
 
 
-def webhook_challenge(request):
-    """
-    A webhook to return a challenge
-    """
-    queryParams = request.args
-    verify_token = queryParams['hub.verify_token']
-    # check whether the verify tokens match
-    if queryParams['hub.mode'] == 'subscribe' and \
-            verify_token == FB_VERIFY_TOKEN:
-        # respond with the challenge to confirm
-        challenge = queryParams['hub.challenge']
-        return challenge
-    else:
-        return 'Invalid Request or Verification Token. Expected FB_PAGE_ACCESS={} and FB_VERIFY_TOKEN={}'.format(FB_PAGE_ACCESS,FB_VERIFY_TOKEN)
 
 
-def messenger_post(request):
+
+def process_messages_event(request):
     """
     Handler for webhook (currently for postback and messages)
     """
+    try:
+        data = request.json
+    except Exception as e:
+        return "Unable to parse json. Received error {}.".format(e)
 
-    data = request.json
-    if data['object'] == 'page':
-        for entry in data['entry']:
-            # get all the messages
-            messages = entry['messaging']
-            if messages[0]:
-                # Get the first message
-                message = messages[0]
-                # Yay! We got a new message!
-                # We retrieve the Facebook user ID of the sender
-                fb_id = message['sender']['id']
-                if fb_id == '<PSID>':
-                    raise ValueError("Invalid FB user ID: {}".format(fb_id))
-                # We retrieve the message content
-                text = message['message']['text']
+    try:
+        if data['object'] == 'page':
+            for entry in data['entry']:
+                messaging = entry['messaging']
+                if len(ms) > 0 and messaging[0]:
+                    m = messaging[0]
+                    fb_id = m['sender']['id']
 
-                turn_on_seen_and_typing_indicator(fb_id)
+                    if 'text' in m['message'].keys():
+                        handle_text(m['message']['text'])
+                    elif 'attachments' in m['message'].keys():
+                        handle_attachment(m['message']['attachments'])
+                    
+                    turn_on_seen_and_typing_indicator(fb_id)
 
-                # Let's forward the message to Wit /message
-                # and customize our response to the message in handle_message
-                response = Wit(access_token=WIT_TOKEN).message(msg=text, context={'session_id':fb_id}, verbose=True)
-                handle_message(response=response, fb_id=fb_id)
-    else:
-        # Returned another event
-        return 'Received Different Event'
+    except Exception as e:
+        return "Unable to decipher json. Received error {}.".format(e)
+
     return 'OK'
 
-def app_secret_proof():
-    return hmac.new(FB_PAGE_ACCESS_2.encode('utf-8'),msg=FB_PAGE_ACCESS.encode('utf-8'),digestmod=hashlib.sha256).hexdigest()
+
+def handle_text(text):
+    response = Wit(access_token=WIT_TOKEN).message(
+        msg=text, context={'session_id':fb_id}, verbose=True)
+    handle_message(response=response, fb_id=fb_id)
 
 
 def fb_message(sender_id, text):
@@ -102,13 +110,8 @@ def fb_message(sender_id, text):
         'recipient': {'id': sender_id},
         'message': {'text': text}
     }
-
-    # Setup the query string with your PAGE TOKEN
-    qs = 'access_token=' + FB_PAGE_ACCESS + \
-        '&appsecret+proof=' + app_secret_proof() 
-    # Send POST request to messenger
-    resp = requests.post(FB_MESSAGES_URL + qs,
-                         json=data)
+    
+    resp = req.post(MESSAGES_URL, json=data)
 
 
 def first_entity_value(entities, entity):
@@ -132,7 +135,7 @@ def handle_message(response, fb_id):
     # Otherwise we will just repeat what they sent us
     greetings = first_entity_value(entities, 'greetings')
     if greetings:
-        text = "hello!"
+        text = "hello {{user_first_name}}!"
     else:
         text = "We've received your message: " + response['_text']
     # send message
@@ -144,13 +147,22 @@ def turn_on_seen_and_typing_indicator(fb_id):
         'recipient': { 'id': fb_id }
     }
 
-    qs = 'access_token=' + FB_PAGE_ACCESS + \
-        '&appsecret+proof=' + app_secret_proof() 
-
-    data['sender_action'] = 'typing_on'
-    resp = requests.post(FB_MESSAGES_URL+qs, json=data)
     data['sender_action'] = 'mark_seen'
-    resp = requests.post(FB_MESSAGES_URL+qs, json=data)
+    resp = req.post(MESSAGES_URL, json=data)
+    data['sender_action'] = 'typing_on'
+    resp = req.post(MESSAGES_URL, json=data)
+    
+
+
+def get_id_name(fb_id):
+    data = {'fields':['first_name','last_name']}
+    resp = req.get(GRAPH_API+AUTH+fb_id+'?', json=data)
+    if "error" in resp.keys():
+        return None
+    else: return resp['first_name'], resp['last_name']
+
+
+
 
 
 if __name__ == '__main__':
