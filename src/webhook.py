6649e578673591e4b111607bd70ee1347fd50e51
Anthony Ho <anwyho@gmail.com>
2018-09-02 01:22:22 -0700

    Added logging

--- src/webhook.py
+++ src/webhook.py
@@ -6,8 +6,8 @@ import sys
 
 from flask import Flask, request
 
+from .events import process_event
 from .getChallenge import verify_challenge
-from .processMessages import process_messages_event
 
 app = Flask(__name__)
 
@@ -15,21 +15,28 @@ app = Flask(__name__)
 @app.route("/")
 def main_handle():
     """Returns static website for Bartbot info"""
-    log.info("In main handle")
     # TODO turn this into staticly delivered website
     return """Hello! This is the main API endpoint for Bartbot. What is Bartbot you ask? Check out <a href=\"http://github.com/anwyho/bartbot\">github.com/anwyho/bartbot</a> for more details."""
 
 
 @app.route("/webhook", methods=['POST', 'GET'])
 def handle_webhook():
-    log.info("In webhook")
+    log.basicConfig(filename='bartbot-info.log', format="%(levelname)s:%(module)s.%(funcName)s:%(lineno)d %(message)s:%(asctime)s", level=log.INFO)
+    # log.basicConfig(filename='bartbot-debug.log', level=log.DEBUG)
+    log.info("S T A R T I N G   L O G")
     # TODO: Verify SHA-1
+    res = None
     if request.method == 'GET':
-        return verify_challenge(request)
+        res = verify_challenge(request)
     elif request.method == 'POST':
-        return process_messages_event(request)
+        res = process_event(request)
     else:
-        return "Unsupported HTTPS Verb."
+        res = "Unsupported HTTPS Verb."
+
+    log.info("E N D I N G   L O G")
+    log.shutdown()
+    return res
+    
 
 
 if __name__ == '__main__':
