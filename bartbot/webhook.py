a4c11d7d803099d4b0b2123ca5c8ecbe626ce944
Anthony Ho <anwyho@gmail.com>
2018-09-04 01:08:30 -0700

    Improved logging

--- bartbot/webhook.py
+++ bartbot/webhook.py
@@ -1,13 +1,17 @@
 # from __future__ import absolute_import
 # from __future__ import print_function
 
+# TODO: File-level docstrings 
+
+
 import logging
+import os
 # TODO: Look into logging that filters out sensitive info
 
 from flask import (Flask, request)
 
-from .events import process_event
-from .getChallenge import verify_challenge
+from .events.event import process_event
+from .challenge import verify_challenge
 
 app = Flask(__name__)
 
@@ -25,27 +29,57 @@ def main_handle():
 def handle_webhook():
     """Processes POST and GET requests from the Messenger Platform"""
 
+    # Set up logging configuration
+    logFile = os.path.join( '.',
+        'bartbot',
+        '.logs', 
+        '.bartbot-{}.log'.format('debug' if DEBUG else 'info'))
+    os.makedirs(os.path.dirname(logFile), exist_ok=True)
+    logFormat = "%(levelname)s:%(module)s:%(lineno)d %(message)s:%(asctime)s"
+
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
     logging.basicConfig(
-        filename='.bartbot-{}.log'.format('debug' if DEBUG else 'info'), 
-        format="%(levelname)s:%(module)s.%(funcName)s:%(lineno)d %(message)s:%(asctime)s", 
-        level=logging.DEBUG if DEBUG else logging.info)
+        filename=logFile, 
+        format=logFormat, 
+        level=logging.DEBUG if DEBUG else logging.INFO)
+
     logging.info("S T A R T I N G   L O G")
-    
-    # TODO: Verify SHA-1
 
     res = None
+    if verify_request(request):
+        res = process_request(request)
+    else:
+        res = "Invalid request. Failed SHA-1 verification."
+
+    logging.info("E N D I N G   L O G")
+    logging.shutdown()
+
+    return res+'\n'
+
+
+def process_request(request) -> str:
+    """
+    Passes request onto other methods depending on request method
+    """
+    
     if request.method == 'GET':
         res = verify_challenge(request)
     elif request.method == 'POST':
         res = process_event(request)
     else:
         res = "Unsupported HTTPS Verb."
-
-    logging.info("E N D I N G   L O G")
-    logging.shutdown()
+        
     return res
+
+
+def verify_request(request) -> bool:
+    """Verifies SHA-1 of entire message"""
+    # TODO: Verify SHA-1
+    return True
 
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
