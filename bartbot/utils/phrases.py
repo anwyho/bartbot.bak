eff2f4d26e9d111df839347b99e97dca7f8b632f
Anthony Ho <anwyho@gmail.com>
2018-09-05 01:52:14 -0700

    Added emoji support and tests

--- bartbot/utils/phrases.py
+++ bartbot/utils/phrases.py
@@ -1,10 +1,11 @@
-# from __future__ import print_function
-# from __future__ import unicode_literals
-
 import random as r
 import time
 
-def get_phrase(*sentences, opt='{opt}'):
+from typing import List
+
+from .emojis import emojis
+
+def get_phrase(*sentences:List[str], opt:str='{opt}') -> str:
     """Constructs randomized sentences of phrase lists"""
     resp = '{opt}'
     while '{opt}' in resp:
@@ -12,7 +13,7 @@ def get_phrase(*sentences, opt='{opt}'):
     return resp
 
 
-def time_of_day(night=True):
+def time_of_day(night:bool=True) -> str:
     """Returns signifier for the time of day"""
     hour = time.localtime().tm_hour 
     if night and (hour > 21 or hour <= 4):
@@ -28,86 +29,110 @@ def time_of_day(night=True):
 
 
 # Phrase lists
-
-hello = [
-    'Greetings.',
-    'Hello there.',
-    'Sup!',
-    'Sup {opt}!',
-    'Hello from the other side!',
-    'Good day!', 
-    'Good day, {opt}!',
-    f'Good {time_of_day(night=False)}!',
-    'Hi there!',
-    'Hello!',
-    'Hello {opt}!',
-    'Hi!',
-    'Hi {opt}!',
-    ]
-cta = [
-    'Where are you headed?',
-    'Where are you headed today?',
-    'Where would you like to go?',
-    'Where would you like to go today?',
-    'Where are you off to?',
-    'Where are you off to today?',
-    'Where to?',
-    'Where ya headed?',
-    'Where ya headed today?',
-    ]
-sorry = [
-    'Sorry about that! {opt}',
-    'Oops! {opt}',
-    'Oh no! {opt}',
-    'Hmm... {opt}',
-    'Darn. {opt}',
-    'Shucks. {opt}',
-    'That\'s embarrassing... {opt}',
-    'Aiya! {opt}',
-    'Whoops! {opt}',
-    ''
-]
-yw = [
-    'You\'re welcome!',
-    f'Have a great {time_of_day()}!',
-    'Anytime!',
-    'Yeah, no problem!',
-    'Safe travels!',
+attachments = [
+    f"Ooh attachment! I'm starting to get a little attached to you too {{opt}} {emojis['smiling_face_with_smiling_eyes']}",
+    "Ooh! For me?",
+    "Wow!",
+    "Hmm... I don\'t quite know what to do with this.",
+    "Hello there.",
+    "Whoa what is that?",
+    ""
     ]
 bye = [
-    'See ya later!',
-    'See ya later {opt}!',
-    'Adios!',
-    'Bye bye!',
-    'Zai jian!',
-    'Take care.',
-    'Love you!',
-    'BART safe!',
-    'Bye!',
-    'Bye {opt}!',
-    'Tootles!',
-    'TTFN',
-    'TTYL',
-    'TTYL {opt}!',
-    'Later!',
-    'Until next time!',
-    'Until next time, {opt}!',
+    "See ya later!",
+    "See ya later {opt}!",
+    "Adios!",
+    "Bye bye!",
+    "Zai jian!",
+    "Take care.",
+    "Love you!",
+    "BART safe!",
+    "Bye!",
+    "Bye {opt}!",
+    "Tootles!",
+    "TTFN",
+    "TTYL",
+    "TTYL {opt}!",
+    "Later!",
+    "Until next time!",
+    "Until next time, {opt}!",
+    ]
+# CTA usually follows hello. 
+# NOTE: No {opt}s because name would"ve already been stated.
+cta = [
+    "Where are you headed?",
+    "Where are you headed today?",
+    "Where would you like to go?",
+    "Where would you like to go today?",
+    "Where are you off to?",
+    "Where are you off to today?",
+    "Where to?",
+    "Where ya headed?",
+    "Where ya headed today?",
     ]
 delivery = [
-    'Here it is!',
-    'Here it is {opt}!',
-    'Here you go!',
-    'Here ya go {opt}!',
-    'Here ya go!',
-    'There ya go!',
-    'Special delivery!',
-    'Special delivery, for {opt}',
-]
+    "Here it is!",
+    "Here it is {opt}!",
+    "Here you go!",
+    "Here ya go {opt}!",
+    "Here ya go!",
+    "There ya go!",
+    "Special delivery!",
+    "Special delivery, for {opt}",
+    ]
+hello = [
+    "Greetings.",
+    "Hello there.",
+    "Sup!",
+    "Sup {opt}!",
+    "Hello from the other side!",
+    "Good day!", 
+    "Good day, {opt}!",
+    f"Good {time_of_day(night=False)}!",
+    f"Good {time_of_day(night=False)} {{opt}}!",
+    "Hi there!",
+    "Hello!",
+    "Hello {opt}!",
+    "Hi!",
+    "Hi {opt}!",
+    "Hey, didn\'t see ya there!"
+    ]
+sorry = [
+    "Sorry about that!",
+    "Oops!",
+    "Oh no!",
+    "Hmm...",
+    "Darn.",
+    "Shucks.",
+    "That\'s embarrassing...",
+    "Aiya!",
+    "Whoops!",
+    ""
+    ]
+thanks = [
+    "Thanks!",
+    "Thankya!",
+    "Wow thanks!",
+    "tysm!",
+    "TY!",
+    "Xie xie!",
+    ]
+yw = [
+    "You\'re welcome!",
+    f"Have a great {time_of_day()}!",
+    "Anytime!",
+    "Yeah, no problem!",
+    "Safe travels!",
+    ]
+
+
+
+
 
 
 
 if __name__ == '__main__':
-    print('Phrases demonstration:')
+    print("Phrases demonstration:")
     print(get_phrase(hello,cta))
     print(get_phrase(yw,bye))
 
