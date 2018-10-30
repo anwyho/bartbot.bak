31b1ad4aac2c9ee7a00389136e65c302577e8534
Anthony Ho <anwyho@gmail.com>
2018-10-29 23:04:59 -0700

    Separated controller into different file

--- bartbot/process/controller.py
+++ bartbot/process/controller.py
@@ -1,22 +1,13 @@
-# import boto3
-import json
 import logging
-import wrapt
 
 from abc import (ABC, abstractmethod)
-from typing import(Any, Dict, List, Optional, Tuple, Union)
 
-from bartbot.process.user import (User)
 from bartbot.receive.message import (Message)
-from bartbot.receive.attachment import (Attachment as Attachment)
-from bartbot.receive.postback import (Postback)
-from bartbot.receive.referral import (Referral)
-from bartbot.receive.text import (Text)
-from bartbot.send.attachment import (Asset, Template)
-from bartbot.send.button import (Button)
 from bartbot.send.response import (Response, ResponseBuilder)
-from bartbot.resources.map import (get_map_id)
-from bartbot.utils.phrases import (Phrase)
+
+
+def import_controller(controllerName: str):
+    pass
 
 
 class Controller(ABC):
@@ -34,51 +25,9 @@ class Controller(ABC):
 
 class EchoController(Controller):
     """Calling produce_responses from this class sends an echo of the received message."""
-    pass
 
-
-class BartbotEnController(Controller):
-    def produce_responses(self) -> Response:
-        head = ResponseBuilder(recipientId=self.message.senderId)
-        nextResp = head
-
-        if isinstance(self.message, Attachment):
-            nextResp.text = self.message._phrase.get_phrase(
-                'attachment', opt={'fn': self.message._client.fn})
-
-        elif isinstance(self.message, Text):
-            nextResp.text = f"You typed {self.message.text}\n"
-            nextResp = nextResp.make_chained_response(
-                text=f"Debug info: \n{json.dumps(self.message.entities, indent=2)}")
-
-            if self.message.entities.get('greetings', [{}])[0].get(
-                    'confidence', 0) > 0.7:
-
-                nextResp = nextResp.make_chained_response(
-                    text=self.message._phrase.get_phrase(
-                        'hello', 'cta', opt={'fn': self.message._client.fn}))
-
-            # if self.message.entities.get('map', [{}])[0].get(
-                # 'confidence', 0) > 0.7:
-            if 'map' == self.message.entities.get('intent', [{}])[0].get('value', ''):
-
-                nextResp = nextResp.make_chained_response(
-                    text=self.message._phrase.get_phrase(
-                        'delivery', opt={'fn': self.message._client.fn}))
-                mapId = get_map_id()
-                if mapId:
-                    nextResp = nextResp.make_chained_response(
-                        attachment=Asset(assetType='image', attchId=mapId))
-                else:
-                    # TODO: Backup plan
-                    pass
-
-                nextResp.add_quick_reply(
-                    text="Break Bartbot", postbackPayload="Payload")
-                nextResp.add_quick_reply(
-                    text="Break Bartbot Pt. 2", postbackPayload="Payload")
-
-        return head
+    def produce_responses(self) -> ResponseBuilder:
+        return ResponseBuilder(recipientId=self.message.senderId, text=f"You typed {self.message.text if hasattr(self.message, 'text') else '[no text found]'}")
 
 
 # TODO:
