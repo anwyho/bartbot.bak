7319f3a1b1c76011155ac4d6a828abaebb6c0203
Anthony Ho <anwyho@gmail.com>
2018-10-29 23:14:34 -0700

    Modified how getting map works

--- bartbot/resources/map.py
+++ bartbot/resources/map.py
@@ -1,41 +1,42 @@
 import json
-import logging 
+import logging
 import os
 
 from typing import Optional
 
-from ..utils.requests import post
-from ..utils.urls import MESSAGE_ATTACHMENTS_API
+from bartbot.send.response import (Response)
+from bartbot.utils.urls import MESSAGE_ATTACHMENTS_API
 
 
-BART_MAP_URL="https://github.com/anwyho/bart-map/blob/master/BART_cc_map.png?raw=true"
-BART_MAP_FILE = os.path.join('bartbot', 'resources', 'images', 'bart_map_id.txt')
+BART_MAP_URL = "https://github.com/anwyho/bart-map/blob/master/BART_cc_map.png?raw=true"
+BART_MAP_FILE = os.path.join(
+    'bartbot', 'resources', 'images', 'bart_map_id.txt')
 
 # HACK: Improve this function to work for any given attachment
 
-def get_map_id(forceRefresh=False, writeCache=BART_MAP_FILE) -> Optional[str]:
+
+def yield_map_id(forceRefresh=False, writeCache=BART_MAP_FILE) -> Optional[str]:
     """
-    Saves attachment to FB for cached map. 
+    Yield local copy of map ID and then a refreshed version.
+    Saves attachment to FB for cached map.
     Places attachment ID into 'bartbot/resources/images/bart_map_id.txt'.
     """
 
-    mapId = None
-    
-    if not forceRefresh: 
-        mapId = read_local()
+    # Yields locally sourced map ID
+    mapId = read_local()
+    yield mapId
 
     # Upload attachment from GitHub URL
-    if mapId is None:  
-        mapId = post_from_url()
-        if mapId is not None: 
-            write_local(mapId)
+    mapId = post_from_url()
+    if mapId is not None:
+        write_local(mapId)
 
     # TODO: Upload attachment from S3 Bucket
     if mapId is None:
         mapId = post_from_S3()
 
-    return mapId
-    
+    yield mapId
+
 
 def read_local() -> Optional[str]:
     """Tries to read attachment ID from file"""
@@ -44,8 +45,8 @@ def read_local() -> Optional[str]:
             mapId = f.read()
 
     except IOError as e:
-        logging.warning("Couldn't retreive attachment ID from file " +  
-            f"{BART_MAP_FILE}. Error: {e}")
+        logging.warning("Couldn't retreive attachment ID from file " +
+                        f"{BART_MAP_FILE}. Error: {e}")
         mapId = None
 
     else:
@@ -55,23 +56,22 @@ def read_local() -> Optional[str]:
             logging.warning("Invalid attachment ID in file")
             logging.debug(f"mapId = {mapId}")
             mapId = None
-            
-    finally: 
+
+    finally:
         return mapId
 
 
 def post_from_url() -> Optional[str]:
     """Tries to POST url of original map to Messenger Attachments"""
     data = {
-    "message": {
-        "attachment": {
-        "type": "image", 
-        "payload": {
-            "is_reusable": 'true',
-            "url": BART_MAP_URL }}}}
-
-    ok, resp = post(MESSAGE_ATTACHMENTS_API, json=data)
-    if ok and 'attachment_id' in resp:
+        "message": {
+            "attachment": {
+                "type": "image",
+                "payload": {
+                    "is_reusable": 'true',
+                    "url": BART_MAP_URL}}}}
+    isOk, resp = Response(apiUrl=MESSAGE_ATTACHMENTS_API, data=data).send()[0]
+    if isOk and 'attachment_id' in resp:
         return resp['attachment_id']
     else:
         logging.warning("Couldn't retrieve attachment ID from Messenger")
@@ -79,25 +79,24 @@ def post_from_url() -> Optional[str]:
         return None
 
 
-def write_local(mapId:str) -> bool:
+def write_local(mapId: str) -> bool:
     """Tries to cache attachment ID to a local file"""
-    try: 
+    try:
         with open(BART_MAP_FILE, 'w') as f:
             f.write(mapId)
     except Exception as e:
-        logging.warning("Couldn't write attachment ID to " +  
-            f"file {BART_MAP_FILE}. Received error {e}.")
+        logging.warning("Couldn't write attachment ID to " +
+                        f"file {BART_MAP_FILE}. Received error {e}.")
         return False
-    else: 
+    else:
         logging.info(f"Wrote map attachment ID to file {BART_MAP_FILE}")
         return True
 
 
 def post_from_S3() -> Optional[str]:
     # TODO: Not implemented
-    return None
-
+    raise NotImplementedError()
 
 
 if __name__ == '__main__':
-    get_map_id(forceRefresh=True)
+    print(yield_map_id(forceRefresh=True))
