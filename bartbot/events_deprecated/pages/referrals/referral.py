303faf77382fb8a70bf079d586d85e8317cae93a
Anthony Ho <anwyho@gmail.com>
2018-10-30 00:02:34 -0700

    Removed deprecated files

--- bartbot/events_deprecated/pages/referrals/referral.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# TODO: This entire file
-
-def handle_referral(fbId:str, referral:dict, respMsg:str) -> str:
-    respMsg += "Received referral object. Referral is not currently supported.\n"
-    return respMsg
-
-
