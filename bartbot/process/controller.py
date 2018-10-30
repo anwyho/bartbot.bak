317dabdcad402e6ec4fda36763053da7a2880c79
Anthony Ho <anwyho@gmail.com>
2018-10-29 20:32:06 -0700

    Controller creates equivalent functionality as master branch

--- bartbot/process/controller.py
+++ bartbot/process/controller.py
@@ -6,22 +6,25 @@ import wrapt
 from abc import (ABC, abstractmethod)
 from typing import(Any, Dict, List, Optional, Tuple, Union)
 
+from bartbot.process.user import (User)
 from bartbot.receive.message import (Message)
-from bartbot.receive.attachment import (Attachment)
+from bartbot.receive.attachment import (Attachment as Attachment)
 from bartbot.receive.postback import (Postback)
 from bartbot.receive.referral import (Referral)
 from bartbot.receive.text import (Text)
+from bartbot.send.attachment import (Asset, Template)
+from bartbot.send.button import (Button)
 from bartbot.send.response import (Response, ResponseBuilder)
-from bartbot.process.user import (User)
+from bartbot.resources.map import (get_map_id)
 from bartbot.utils.phrases import (Phrase)
 
 
-class Controller:
+class Controller(ABC):
     def __init__(self, message: Message) -> None:
         self.message: Message = message
 
     @abstractmethod
-    def produce_response(self) -> Response:
+    def produce_responses(self) -> Response:
         """
         Parse the message and generate a response to send using the
             ResponseBuilder.
@@ -29,16 +32,53 @@ class Controller:
         pass
 
 
-class BartbotController(Controller):
-    def produce_response(self) -> Response:
-        resp = ResponseBuilder(recipientId=self.message.senderId)
+class EchoController(Controller):
+    """Calling produce_responses from this class sends an echo of the received message."""
+    pass
+
+
+class BartbotEnController(Controller):
+    def produce_responses(self) -> Response:
+        head = ResponseBuilder(recipientId=self.message.senderId)
+        nextResp = head
 
         if isinstance(self.message, Attachment):
-            resp.text = "# TODO: Get phrase"
-        elif isinstance(self.message, Text):
-            resp.make_template()
+            nextResp.text = self.message._phrase.get_phrase(
+                'attachment', opt={'fn': self.message._client.fn})
 
-        return resp
+        elif isinstance(self.message, Text):
+            nextResp.text = f"You typed {self.message.text}\n"
+            nextResp = nextResp.make_chained_response(
+                text=f"Debug info: \n{json.dumps(self.message.entities, indent=2)}")
+
+            if self.message.entities.get('greetings', [{}])[0].get(
+                    'confidence', 0) > 0.7:
+
+                nextResp = nextResp.make_chained_response(
+                    text=self.message._phrase.get_phrase(
+                        'hello', 'cta', opt={'fn': self.message._client.fn}))
+
+            # if self.message.entities.get('map', [{}])[0].get(
+                # 'confidence', 0) > 0.7:
+            if 'map' == self.message.entities.get('intent', [{}])[0].get('value', ''):
+
+                nextResp = nextResp.make_chained_response(
+                    text=self.message._phrase.get_phrase(
+                        'delivery', opt={'fn': self.message._client.fn}))
+                mapId = get_map_id()
+                if mapId:
+                    nextResp = nextResp.make_chained_response(
+                        attachment=Asset(assetType='image', attchId=mapId))
+                else:
+                    # TODO: Backup plan
+                    pass
+
+                nextResp.add_quick_reply(
+                    text="Break Bartbot", postbackPayload="Payload")
+                nextResp.add_quick_reply(
+                    text="Break Bartbot Pt. 2", postbackPayload="Payload")
+
+        return head
 
 
 # TODO:
