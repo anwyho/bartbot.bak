ad49a3cf55fb5be0492a9427e4b58bd89d2d96f0
Anthony Ho <anwyho@gmail.com>
2018-11-05 13:46:46 -0800

    Updating intent handling and response building

--- bartbot/process/bartbot_controller.py
+++ bartbot/process/bartbot_controller.py
@@ -1,6 +1,7 @@
 # import boto3
 import json
 import logging
+import time
 import wrapt
 
 from abc import (ABC, abstractmethod)
@@ -8,66 +9,118 @@ from typing import(Any, Dict, List, Optional, Tuple, Union)
 
 from bartbot import receive as rcv
 from bartbot.process.controller import (Controller)
+from bartbot.process.entities import (WitEntities)
 from bartbot.send.attachment import (Asset, Template)
 from bartbot.send.button import (Button)
-from bartbot.send.response import (Response, ResponseBuilder)
+from bartbot.send.response import (Response)
+from bartbot.send.response_builder import (ResponseBuilder)
+from bartbot.receive.attachment import (Attachment)
 from bartbot.resources.map import (yield_map_id)
 
 
 class BartbotController(Controller):
+
+    HELP_TEXT = "[TODO: Fill in this help text.]"
+
     def produce_responses(self) -> ResponseBuilder:
         head = ResponseBuilder(recipientId=self.message.senderId)
-        nextResp = head
+        respTail = head
 
-        if isinstance(self.message, rcv.attachment.Attachment):
-            nextResp.text = self.message._phrase.get_phrase(
+        head.text = f'You typed: "{self.message.text}"'
+        head.description = "Echoing message"
+
+        if isinstance(self.message, Attachment):
+            respTail = respTail.make_chained_response()
+            respTail.text = self.message._phrase.get_phrase(
                 'attachment', opt={'fn': self.message._client.fn})
+            respTail.description = "Attachment response"
 
         elif isinstance(self.message, rcv.text.Text):
-            nextResp.text = f"You typed {self.message.text}\n"
-            # nextResp = nextResp.make_chained_response(
-            #     text=f"Debug info: \n{json.dumps(self.message.entities, indent=2)}")
+            respTail = respTail.make_chained_response()
+            entities = WitEntities(self.message.entities)
+            respTail.text = str(entities)
+            respTail.description = "Wit entities"
 
-            if self.message.entities.get('greetings', [{}])[0].get(
-                    'confidence', 0) > 0.7:
+            intent = entities.intent
+            if 'help' == intent:
+                respTail = self.help_response(respTail)
+            elif 'map' == intent:
+                respTail = self.map_response(respTail)
+            elif 'travel' == intent:
+                respTail = self.travel_response(respTail)
+            elif 'single-trip-cost' == intent:
+                respTail = self.cost_response(respTail)
+            elif 'round-trip-cost' == intent:
+                respTail = self.cost_response(respTail, roundTrip=True)
+            elif 'weather' == intent:
+                respTail = self.weather_response(respTail)
+            elif 'reset' == intent:
+                respTail = self.reset_response(respTail)
+            else:
+                pass
 
-                nextResp = nextResp.make_chained_response(
-                    text=self.message._phrase.get_phrase(
-                        'hello', 'cta', opt={'fn': self.message._client.fn}))
-
-            # if self.message.entities.get('map', [{}])[0].get(
-                # 'confidence', 0) > 0.7:
-            if 'map' == self.message.entities.get('intent', [{}])[0].get('value', ''):
-                nextResp = self.intent_map(nextResp)
-
-            nextResp.add_quick_reply(
-                text="Break Bartbot", postbackPayload="Payload")
-            nextResp.add_quick_reply(
-                text="Break Bartbot Pt. 2", postbackPayload="Payload")
+            respTail.add_quick_reply(
+                text="What is love?", postbackPayload="Payload")
+            respTail.add_quick_reply(
+                text="Baby don't hurt me...", postbackPayload="Payload")
 
         return head
 
-    def send_waiting_response(self, responseSkeleton):
-        responseSkeleton.text = self.message._phrase.get_phrase(
-            'wait', opt={'fn': self.message._client.fn})
-        responseSkeleton.make_chained_response(senderAction="typing_on")
-        responseSkeleton.send()
+    def help_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
+        respTail = respTail.make_chained_response(
+            text=self.HELP_TEXT,
+            description="Help text response")
+        return respTail
 
-    def intent_map(self, nextResp: ResponseBuilder) -> ResponseBuilder:
-        nextResp = nextResp.make_chained_response(
+    def map_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
+        respTail = respTail.make_chained_response(
             text=self.message._phrase.get_phrase(
-                'delivery', opt={'fn': self.message._client.fn}))
+                'delivery', opt={'fn': self.message._client.fn}),
+            description="Delivery text")
         mapIdGen = yield_map_id()
         mapId = next(mapIdGen)
         if not mapId:
-            self.send_waiting_response(
-                responseSkeleton=nextResp.make_separate_response())
+            self.send_waiting_response(respTail)
             mapId = next(mapIdGen)
         if mapId:
-            nextResp = nextResp.make_chained_response(
-                attachment=Asset(assetType='image', attchId=mapId))
+            respTail = respTail.make_chained_response(
+                attachment=Asset(assetType='image', attchId=mapId),
+                description="Map asset from attachment ID")
         else:
             # TODO: Backup plan
             pass
 
-        return nextResp
+        return respTail
+
+    def cost_response(self, respTail: ResponseBuilder, roundTrip: bool = False) -> ResponseBuilder:
+        respTail.make_chained_response(
+            text="[TODO: Fill in the cost response.]",
+            description="Cost text")
+        return respTail
+
+    def travel_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
+        respTail.make_chained_response(
+            text="[TODO: Fill in the travel response.]",
+            description="Travel text")
+        return respTail
+
+    def weather_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
+        respTail.make_chained_response(
+            text="[TODO: Fill in the weather response.]",
+            description="Weather text")
+        return respTail
+
+    def reset_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
+        respTail.make_chained_response(
+            text="[TODO: Fill in the reset response.]",
+            description="Reset text")
+        return respTail
+
+    def send_waiting_response(self, respTail: ResponseBuilder):
+        respBranch = respTail.make_separate_response(description="Wait text")
+        respBranch.text = self.message._phrase.get_phrase(
+            'wait', opt={'fn': self.message._client.fn})
+        respBranch.make_chained_response(
+            senderAction="typing_on",
+            description="Turn typing on")
+        respBranch.send()
