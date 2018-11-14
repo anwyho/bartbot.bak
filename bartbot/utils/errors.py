fd2993cc96542fce5dfa20f6416618b5b608ca78
Anthony Ho <anwyho@gmail.com>
2018-11-14 10:32:22 -0800

    Clarified variable names

--- bartbot/utils/errors.py
+++ bartbot/utils/errors.py
@@ -18,7 +18,7 @@ def handle_request_error(response: dict) -> bool:
         return True
 
 
-def print_traceback(e):
+def print_traceback(error):
     import os
     import sys
     import traceback
@@ -26,5 +26,6 @@ def print_traceback(e):
     traceback.print_tb(exc_tb)
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     logging.debug(
-        f"Error: {e}. Error info: {exc_type}, {fname}, {exc_tb.tb_lineno}")
-    print(f"Error: {e}. Error info: {exc_type}, {fname}, {exc_tb.tb_lineno}")
+        f"Error: {error}. Error info: {exc_type}, {fname}, {exc_tb.tb_lineno}")
+    print(
+        f"Error: {error}. Error info: {exc_type}, {fname}, {exc_tb.tb_lineno}")
