83ff88f54eb6f57f992e21ec8f15f83a5870c570
Anthony Ho <anwyho@gmail.com>
2018-11-14 10:30:48 -0800

    Cut pre- and post-processing from event and disambiguated handle loop

--- bartbot/receive/event.py
+++ bartbot/receive/event.py
@@ -11,11 +11,13 @@ from typing import (Generator, List, Optional, Tuple, Type, TypeVar)
 # from bartbot.receive.postback import Postback
 # from bartbot.receive.referral import Referral
 # from bartbot.receive.text import Text
+from bartbot.receive.message import (MessageParsingError)
 from bartbot.process.controller import (EchoController)
 from bartbot.process.bartbot_controller import (BartbotController)
 # from bartbot.process.controller import (import_controller)
 from bartbot.send.response import (Response)
 from bartbot.send.response_builder import (ResponseBuilder)
+from bartbot.utils.errors import (print_traceback)
 
 
 def process_event(req: request) -> list:
@@ -45,13 +47,17 @@ def process_event(req: request) -> list:
 
 def handle_page_event(entry: dict):
     """For each message in an entry, create and send a response."""
-    # BUG: from_message() doesn't return None, but throws KeyError. If thrown, the other messages don't get processed.
-    # TODO: Turn this method into a nice for loop maybe
-    return list((Response.from_message(
-        message=message,
-        controllerType=BartbotController)  # TODO: Get from YAML
-        .send()
-        for message in get_messages(entry)))
+    results = []
+    for message in get_messages(entry):
+        try:
+            results.append(Response.from_message(
+                message=message,
+                controllerType=BartbotController)  # TODO: Get from YAML
+                .send())
+        except Exception as e:
+            print_traceback(e)
+
+    return list(results)
 
 
 def handle_user_event(entry: dict):
@@ -70,59 +76,49 @@ def get_messages(entry: dict):
     messaging = entry.get('messaging')
     if isinstance(messaging, list) and len(messaging):
         for msgNum, message in enumerate(messaging):
-            messageInstance = None
+            messageType, messageInstance = None, None
 
             # Attachments gets precedence because it can also have text
             if 'attachments' in message.get('message', {}):
                 from bartbot.receive.attachment import Attachment
-                messageInstance = Attachment.from_entry(
-                    entry, msgNum)
+                messageType = Attachment
 
-            # Echo gets precedence over Text for the same reason
+            # Echo gets precedence over Text for the same reason as Attachments
             elif message.get('message', {}).get('is_echo'):
                 # TODO?
                 # from bartbot.receive.echo import Echo
-                # messageInstance = Echo.from_entry(entry, msgNum)
+                # messageType = Echo
                 pass
 
             elif 'text' in message.get('message', {}):
                 from bartbot.receive.text import Text
-                messageInstance = Text.from_entry(
-                    entry, msgNum)
+                messageType = Text
 
             elif 'postback' in message:
                 from bartbot.receive.postback import Postback
-                messageInstance = Postback.from_entry(
-                    entry, msgNum)
+                messageType = Postback
 
             elif 'referral' in message:
                 from bartbot.receive.referral import Referral
-                messageInstance = Referral.from_entry(
-                    entry, msgNum)
+                messageType = Referral
 
             else:
                 logging.warning(f"Couldn't identify Message type. Skipping.")
                 logging.debug(f"{json.dumps(entry, indent=2)}")
                 pass
 
+            if messageType:
+                try:
+                    messageInstance = messageType(entry=entry, mNum=msgNum)
+                except MessageParsingError as e:
+                    logging.warning(f"Failed to parse message. Error: {e}")
+                    logging.debug(json.dumps(entry))
+                    print_traceback(e)
+                    messageInstance = None
+
             if messageInstance:
                 print("\nReceived message!")
-                seenResponse = ResponseBuilder(
-                    recipientId=messageInstance.senderId,
-                    senderAction="mark_seen",
-                    description="Marking message as seen")
-                seenResponse.make_chained_response(
-                    senderAction="typing_on",
-                    description="Turning typing on")
-                seenResponse.send()
-
                 yield messageInstance
-
-                typingOffResponse = ResponseBuilder(
-                    recipientId=messageInstance.senderId,
-                    senderAction="typing_off",
-                    description="Turning typing off")
-                typingOffResponse.send()
     else:
         logging.warning(f"Couldn't find any page events in entry.")
         logging.debug(f"{json.dumps(entry, indent=2)}")
