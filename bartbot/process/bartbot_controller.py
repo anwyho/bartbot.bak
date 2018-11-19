8b8a6854f1e28c2ac9d33e9434d6cafc27e1bab3
Anthony Ho <anwyho@gmail.com>
2018-11-19 14:40:22 -0800

    Implemented new Phrase interface

--- bartbot/process/bartbot_controller.py
+++ bartbot/process/bartbot_controller.py
@@ -10,29 +10,33 @@ from typing import(Any, Dict, List, Optional, Tuple, Union)
 from bartbot import receive as rcv
 from bartbot.process.controller import (Controller)
 from bartbot.process.entities import (WitEntities)
+from bartbot.process.user import (User)
 from bartbot.send.attachment import (Asset, Template)
 from bartbot.send.button import (Button)
 from bartbot.send.response import (Response)
 from bartbot.send.response_builder import (ResponseBuilder)
 from bartbot.receive.attachment import (Attachment)
 from bartbot.resources.map import (yield_map_id)
+from bartbot.utils.phrases.phrase import (Phrase)
+
+
+DRY_RUN = True
 
 
 class BartbotController(Controller):
 
-    HELP_TEXT = "[TODO: Fill in this help text.]"
-
-    def produce_responses(self) -> ResponseBuilder:
-        self.preprocess_message()
-        response = self.process_message()
-        self.postprocess_message()
-        return response
+    def __init__(self, message, dryRun: bool = False) -> None:
+        super().__init__(message=message, dryRun=dryRun)
+        self.user = User(id=message.senderId)
+        self.phrase = Phrase(initialLocale=self.user.locale)
+        self._dryRun = DRY_RUN
 
     def preprocess_message(self):
         seenResponse = ResponseBuilder(
             recipientId=self.message.senderId,
             senderAction="mark_seen",
-            description="Marking message as seen")
+            description="Marking message as seen",
+            dryRun=self._dryRun)
         seenResponse.create_and_get_chained_response(
             senderAction="typing_on",
             description="Turning typing on")
@@ -42,61 +46,74 @@ class BartbotController(Controller):
         typingOffResponse = ResponseBuilder(
             recipientId=self.message.senderId,
             senderAction="typing_off",
-            description="Turning typing off")
+            description="Turning typing off",
+            dryRun=self._dryRun)
         typingOffResponse.send()
 
     def process_message(self):
-        head = ResponseBuilder(recipientId=self.message.senderId)
-        respTail = head
+        self.phrase.add_attributes(
+            firstName=self.user.fn)
+
+        head = respTail = ResponseBuilder(
+            recipientId=self.message.senderId,
+            dryRun=self._dryRun)
 
         if isinstance(self.message, Attachment):
-            respTail.text = self.message._phrase.get_phrase(
-                'attachment', opt={'fn': self.message._client.fn})
-            respTail.description = "Attachment response"
+            respTail = self.process_attachment(respTail)
 
         elif isinstance(self.message, rcv.text.Text):
-            respTail.text = f'You typed: "{self.message.text}"'
-            respTail.description = "Echoing message"
-            respTail = respTail.create_and_get_chained_response()
-            self.entities = WitEntities(self.message.entities)
-            respTail.text = str(self.entities)
-            respTail.description = "Wit entities"
-
-            intent = self.entities.intent
-            if 'help' == intent:
-                respTail = self.help_response(respTail)
-            elif 'map' == intent:
-                respTail = self.map_response(respTail)
-            elif 'travel' == intent:
-                respTail = self.travel_response(respTail)
-            elif 'single-trip-cost' == intent:
-                respTail = self.cost_response(respTail)
-            elif 'round-trip-cost' == intent:
-                respTail = self.cost_response(respTail, roundTrip=True)
-            elif 'weather' == intent:
-                respTail = self.weather_response(respTail)
-            elif 'reset' == intent:
-                respTail = self.reset_response(respTail)
-            else:
-                pass
-
-            respTail.add_quick_reply(
-                text="What is love?", postbackPayload="Payload")
-            respTail.add_quick_reply(
-                text="Baby don't hurt me...", postbackPayload="Payload")
+            respTail = self.process_text(respTail)
 
         return head
 
+    def process_attachment(self, respTail) -> ResponseBuilder:
+        respTail.text = self.phrase.get(
+            'attachment', useName=True)
+        respTail.description = "Attachment response"
+        return respTail
+
+    def process_text(self, respTail) -> ResponseBuilder:
+        respTail.text = f'You typed: "{self.message.text}"'
+        respTail.description = "Echoing message"
+        self.entities = WitEntities(self.message.entities)
+        respTail = respTail.create_and_get_chained_response(
+            text=str(self.entities),
+            description="Wit entities")
+
+        intent = self.entities.intent
+        if 'help' == intent:
+            respTail = self.help_response(respTail)
+        elif 'map' == intent:
+            respTail = self.map_response(respTail)
+        elif 'travel' == intent:
+            respTail = self.travel_response(respTail)
+        elif 'single-trip-cost' == intent:
+            respTail = self.cost_response(respTail)
+        elif 'round-trip-cost' == intent:
+            respTail = self.cost_response(respTail, roundTrip=True)
+        elif 'weather' == intent:
+            respTail = self.weather_response(respTail)
+        elif 'reset' == intent:
+            respTail = self.reset_response(respTail)
+        else:
+            pass
+
+        respTail.add_quick_reply(
+            text="What is love?", postbackPayload="Payload")
+        respTail.add_quick_reply(
+            text="Baby don't hurt me...", postbackPayload="Payload")
+
+        return respTail
+
     def help_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
         respTail = respTail.create_and_get_chained_response(
-            text=self.HELP_TEXT,
+            text=self.phrase.get('helpText'),
             description="Help text response")
         return respTail
 
     def map_response(self, respTail: ResponseBuilder) -> ResponseBuilder:
         respTail = respTail.create_and_get_chained_response(
-            text=self.message._phrase.get_phrase(
-                'delivery', opt={'fn': self.message._client.fn}),
+            text=self.phrase.get('delivery'),
             description="Delivery text")
         mapIdGen = yield_map_id()
         mapId = next(mapIdGen)
@@ -113,7 +130,7 @@ class BartbotController(Controller):
 
         return respTail
 
-    def cost_response(self, respTail: ResponseBuilder, roundTrip: bool = False) -> ResponseBuilder:
+    def cost_response(self, respTail: ResponseBuilder, roundTrip: bool=False) -> ResponseBuilder:
         respTail.create_and_get_chained_response(
             text="[TODO: Fill in the cost response.]",
             description="Cost text")
@@ -134,8 +151,8 @@ class BartbotController(Controller):
             'orig': self.entities.stn,
             'dest': self.entities.stnDest,
             'time': time.strftime("%-I:%M %p", self.entities.time if self.entities.timeArr is None else self.entities.timeArr),
-            'b': '2',
-            'a': '3',
+            'b': '1',
+            'a': '4',
             'json': 'y',
             'key': BART_PUBL
         }
@@ -148,10 +165,9 @@ class BartbotController(Controller):
         for trip in trips:
             strTrips.append(
                 f"{trip['@origin']} {trip['@origTimeMin']} to {trip['@destination']} {trip['@destTimeMin']}")
-            # respTail = respTail.create_and_get_chained_response(
-            #     text=f"{trip['@origin']} {trip['@origTimeMin']} to {trip['@destination']} {trip['@destTimeMin']}")
         respTail = respTail.create_and_get_chained_response(
-            text='\n'.join(strTrips))
+            text='\n'.join(strTrips),
+            description='HACK trip info')
 
         return respTail
 
@@ -170,8 +186,7 @@ class BartbotController(Controller):
     def send_waiting_response(self, respTail: ResponseBuilder):
         respBranch = respTail.create_and_get_separate_response(
             description="Wait text")
-        respBranch.text = self.message._phrase.get_phrase(
-            'wait', opt={'fn': self.message._client.fn})
+        respBranch.text = self.phrase.get('wait')
         respBranch.create_and_get_chained_response(
             senderAction="typing_on",
             description="Turning typing on for waiting")
