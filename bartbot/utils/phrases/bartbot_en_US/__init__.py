f162a08996fe6802106c1ccfe4c5b750f83c9ab9
Anthony Ho <anwyho@gmail.com>
2018-11-19 14:39:41 -0800

    Modified phrase storage format to fit new Phrase module

--- bartbot/utils/phrases/bartbot_en_US/__init__.py
+++ bartbot/utils/phrases/bartbot_en_US/__init__.py
@@ -3,9 +3,13 @@
 # | '_ \ / _` | '__| __| '_ \ / _ \| __|
 # | |_) | (_| | |  | |_| |_) | (_) | |_
 # |_.__/ \__,_|_|   \__|_.__/ \___/ \__|
-# Get all your BART info from your Messenger app!
+# Get all things BART from your Messenger!
 
-from ..emojis import emojis
+from typing import (Dict, List, Optional)
+
+from bartbot.utils.phrases.emojis import emojis
+
+from bartbot.utils.phrases.bartbot_en_US.attachment import *
 
 # NOTE:
 # 1. Each string should be an f-string
@@ -17,118 +21,173 @@ from ..emojis import emojis
 
 locale = "en_US"
 
-phrases = {
-    'attachment': [
-        f"Ooh attachment! I'm starting to get a little attached to you too {{opt[fn]}} {emojis['smiling_face_with_smiling_eyes']}",
-        f"Ooh attachment! I'm starting to get a little attached to you too {emojis['smiling_face_with_smiling_eyes']}",
-        f"Ooh! For me? {emojis['face_with_open_mouth']}",
-        f"Wow!",
-        f"Hmm... {emojis['face_with_raised_eyebrow']} I don't quite know what to do with this.",
-        f"Hello there. {emojis['beaming_face_with_smiling_eyes']}",
-        f"Whoa what is that?",
-        f"Cool!",
-    ],
-    'bye': [
-        f"See ya later!",
-        f"See ya later {{opt[fn]}}!",
-        f"Adios!",
-        f"Bye bye!",
-        f"Zai jian!",
-        f"Take care.",
-        f"Love you!",
-        f"BART safe!",
-        f"Bye!",
-        f"Bye {{opt[fn]}}!",
-        f"Tootles!",
-        f"TTFN",
-        f"TTYL",
-        f"TTYL {{opt[fn]}}!",
-        f"Later!",
-        f"Until next time!",
-        f"Until next time, {{opt[fn]}}!",
-    ],
-    # CTA usually follows hello.
-    # NOTE: No {opt}s because name would"ve already been stated.
-    # TODO: Seaprate the ones with names into a different category
-    'cta': [
-        f"Where are you headed?",
-        f"Where are you headed today?",
-        f"Where would you like to go?",
-        f"Where would you like to go today?",
-        f"Where are you off to?",
-        f"Where are you off to today?",
-        f"Where to?",
-        f"Where ya headed?",
-        f"Where ya headed today?",
-        f"Headed anywhere?",
-    ],
-    'delivery': [
-        f"Here it is!",
-        f"Here you go!",
-        f"Here ya go {{opt[fn]}}!",
-        f"Here ya go!",
-        f"There ya go!",
-        f"Special delivery!",
-        f"Special delivery, for {{opt[fn]}}",
-    ],
-    'hello': [
-        f"Greetings.",
-        f"Hello there.",
-        f"Sup!",
-        f"Sup {{opt[fn]}}!",
-        f"Hello from the other side!",
-        f"Good day!",
-        f"Good day, {{opt[fn]}}!",
-        f"Good {{opt[time_of_day]}}!",
-        f"Good {{opt[time_of_day]}} {{opt[fn]}}!",
-        f"Hi there!",
-        f"Hello!",
-        f"Hello {{opt[fn]}}!",
-        f"Hi!",
-        f"Hi {{opt[fn]}}!",
-        f"Hey, didn't see ya there!",
-    ],
-    'sorry': [
-        f"Sorry about that!",
-        f"Oops! {emojis['shushing_face']}",
-        f"Oh no. {emojis['face_screaming_in_fear']}",
-        f"Hmm...",
-        f"Darn. {emojis['crying_face']}",
-        f"Shucks. {emojis['crying_face']}",
-        f"That's embarrassing... {emojis['grinning_face_with_sweat']}",
-        f"Aiya!",
-        f"Whoops! {emojis['dizzy_face']}",
-        f"My bad! {emojis['grinning_face_with_sweat']}",
-    ],
-    'thanks': [
-        f"Thanks! {emojis['smiling_face_with_smiling_eyes']}",
-        f"Thanks! {emojis['thumbs_up']}",
-        f"Thanks!",
-        f"Thankya!",
-        f"Wow thanks!",
-        f"tysm!",
-        f"TY!",
-        f"Xie xie!",
-    ],
-    'time-specific emoji': [  # How do I even code this section
-        f"{emojis['sleeping_face']}",
-    ],
-    'wait': [
-        f"One moment please...",
-        f"One sec...",
-        f"BRB!",
-        f"Hold up...",
-        f"Lemme get something...",
-        f"Just a moment!",
-        f"One moment {{opt[fn]}}",
-        f"One sec {{opt[fn]}}...",
-        f"Just a moment {{opt[fn]}}!",
-    ],
-    'yw': [
-        f"You're welcome!",
-        f"Have a great {{opt[time_of_day_w_night]}}!",
-        f"Anytime!",
-        f"Yeah, no problem!",
-        f"Safe travels!",
-    ],
+
+bye: List[str] = [
+    f"See ya later!",
+    f"Adios!",
+    f"Bye bye!",
+    f"Zai jian!",
+    f"Take care.",
+    f"Love you!",
+    f"BART safe!",
+    f"Bye!",
+    f"Tootles!",
+    f"TTFN",
+    f"TTYL",
+    f"Later!",
+    f"Until next time!",
+]
+
+byeWName: List[str] = [
+    f"See ya later {{firstName}}!",
+    f"Bye {{firstName}}!",
+    f"TTYL {{firstName}}!",
+    f"Until next time, {{firstName}}!",
+]
+
+cta: List[str] = [
+    f"Where are you headed?",
+    f"Where are you headed today?",
+    f"Where would you like to go?",
+    f"Where would you like to go today?",
+    f"Where are you off to?",
+    f"Where are you off to today?",
+    f"Where to?",
+    f"Where to, boss?"
+    f"Where ya headed?",
+    f"Where ya headed today?",
+    f"Headed anywhere?",
+]
+
+ctaWName: List[str] = [
+    f"Where are you headed {{firstName}}?",
+    f"Where are you headed today {{firstName}}?",
+    f"Where would you like to go {{firstName}}?",
+    f"Where would you like to go today {{firstName}}?",
+    f"Where are you off to {{firstName}}?",
+    f"Where are you off to today {{firstName}}?",
+    f"Where to, {{firstName}}?",
+    f"Where ya headed {{firstName}}?",
+    f"Where ya headed today {{firstName}}?",
+    f"Headed anywhere {{firstName}}?",
+]
+
+delivery: List[str] = [
+    f"Here it is!",
+    f"Here you go!",
+    f"Here ya go!",
+    f"There ya go!",
+    f"Special delivery!",
+]
+
+deliveryWName: List[str] = [
+    f"Here ya go {{firstName}}!",
+    f"Special delivery, for {{firstName}}",
+    f"Here it is {{firstName}}!",
+    f"There ya go {{firstName}}!",
+]
+
+hello: List[str] = [
+    f"Greetings.",
+    f"Hello there.",
+    f"Sup!",
+    f"Hello from the other side!",
+    f"Good day!",
+    f"Good {{timeOfDayNoNight}}!",
+    f"Hi there!",
+    f"Hello!",
+    f"Hi!",
+    f"Hey, didn't see ya there!",
+]
+
+helloWName: List[str] = [
+    f"Sup {{firstName}}!",
+    f"Good day, {{firstName}}!",
+    f"Good {{timeOfDayNoNight}} {{firstName}}!",
+    f"Hello {{firstName}}!",
+    f"Hi {{firstName}}!",
+]
+
+helpText: List[str] = [
+    f"[TODO: Fill in this help text.]",
+]
+
+sorry: List[str] = [
+    f"Sorry about that!",
+    f"Oops! {emojis['shushing_face']}",
+    f"Oh no. {emojis['face_screaming_in_fear']}",
+    f"Hmm...",
+    f"Darn. {emojis['crying_face']}",
+    f"Shucks. {emojis['crying_face']}",
+    f"That's embarrassing... {emojis['grinning_face_with_sweat']}",
+    f"Aiya!",
+    f"Whoops! {emojis['dizzy_face']}",
+    f"My bad! {emojis['grinning_face_with_sweat']}",
+]
+
+sorryWName: List[str] = [
+    f"Sorry about that {{firstName}}!",
+    f"Sorry {{firstName}}...",
+    f"My bad, {{firstName}}!",
+]
+
+thanks: List[str] = [
+    f"Thanks! {emojis['smiling_face_with_smiling_eyes']}",
+    f"Thanks! {emojis['thumbs_up']}",
+    f"Thanks!",
+    f"Thankya!",
+    f"Wow thanks!",
+    f"tysm!",
+    f"TY!",
+    f"Xie xie!",
+]
+
+thanksWName: List[str] = [
+    f"Thanks {{firstName}}! {emojis['smiling_face_with_smiling_eyes']}",
+    f"Thanks {{firstName}}! {emojis['thumbs_up']}",
+    f"Thanks {{firstName}}!",
+    f"Thankya {{firstName}}!",
+    f"Wow, thanks {{firstName}}!",
+    f"TY {{firstName}}!",
+]
+
+wait: List[str] = [
+    f"One moment please...",
+    f"One sec...",
+    f"BRB!",
+    f"Hold up...",
+    f"Lemme get something...",
+    f"Just a moment!",
+    f"Just a moment...",
+]
+
+waitWName: List[str] = [
+    f"One moment {{firstName}}...",
+    f"One sec {{firstName}}...",
+    f"Just a moment {{firstName}}!",
+    f"Just a moment {{firstName}}...",
+]
+
+yw: List[str] = [
+    f"You're welcome!",
+    f"Have a great {{timeOfDay}}!",
+    f"Anytime!",
+    f"Yeah, no problem!",
+    f"Safe travels!",
+]
+
+ywWName: List[str] = [
+    f"You're welcome {{firstName}}!",
+    f"Have a great {{timeOfDay}} {{firstName}}!",
+    f"Anytime {{firstName}}!",
+    f"Yeah, no problem {{firstName}}!",
+    f"Safe travels, {{firstName}}!",
+]
+
+timeOfDay: dict = {
+    'night': f"night",
+    'evening': f"evening",
+    'afternoon': f"afternoon",
+    'morning': f"morning",
+    'generic': f"day",
 }
