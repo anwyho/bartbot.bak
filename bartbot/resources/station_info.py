133c89637036fdf7fe9f6ee6c3b112f98e26cbd0
Anthony Ho <anwyho@gmail.com>
2018-11-19 14:40:37 -0800

    New station info format

---
+++ bartbot/resources/station_info.py
@@ -0,0 +1,436 @@
+
+
+stations = {
+    "12TH": {
+        "name": "12th St. Oakland City Center",
+        "map-name": "12th St./Oakland City Center",
+        "nickname": "12th St.",
+        "coordinates": [
+            "37.803768",
+            "-122.271450"
+        ]
+    },
+    "16TH": {
+        "name": "16th St. Mission",
+        "map-name": "16th St. Mission",
+        "nickname": "16th St.",
+        "coordinates": [
+            "37.765062",
+            "-122.419694"
+        ]
+    },
+    "19TH": {
+        "name": "19th St. Oakland",
+        "map-name": "19th St./Oakland",
+        "nickname": "19th St.",
+        "coordinates": [
+            "37.808350",
+            "-122.268602"
+        ]
+    },
+    "24TH": {
+        "name": "24th St. Mission",
+        "map-name": "24th St. Mission",
+        "nickname": "24th St.",
+        "coordinates": [
+            "37.752470",
+            "-122.418143"
+        ]
+    },
+    "ANTC": {
+        "name": "Antioch",
+        "map-name": "Antioch",
+        "nickname": "Antioch",
+        "coordinates": [
+            "37.995388",
+            "-121.780420"
+        ]
+    },
+    "ASHB": {
+        "name": "Ashby",
+        "map-name": "Ashby",
+        "nickname": "Ashby",
+        "coordinates": [
+            "37.852803",
+            "-122.270062"
+        ]
+    },
+    "BALB": {
+        "name": "Balboa Park",
+        "map-name": "Balboa Park",
+        "nickname": "Balboa Park",
+        "coordinates": [
+            "37.721585",
+            "-122.447506"
+        ]
+    },
+    "BAYF": {
+        "name": "Bay Fair",
+        "map-name": "Bay Fair",
+        "nickname": "Bay Fair",
+        "coordinates": [
+            "37.696924",
+            "-122.126514"
+        ]
+    },
+    "CAST": {
+        "name": "Castro Valley",
+        "map-name": "Castro Valley",
+        "nickname": "CV",
+        "coordinates": [
+            "37.690746",
+            "-122.075602"
+        ]
+    },
+    "CIVC": {
+        "name": "Civic Center/UN Plaza",
+        "map-name": "Civic Center/UN Plaza",
+        "nickname": "Civic Center",
+        "coordinates": [
+            "37.779732",
+            "-122.414123"
+        ]
+    },
+    "COLS": {
+        "name": "Coliseum",
+        "map-name": "Coliseum",
+        "nickname": "Coliseum",
+        "coordinates": [
+            "37.753661",
+            "-122.196869"
+        ]
+    },
+    "COLM": {
+        "name": "Colma",
+        "map-name": "Colma",
+        "nickname": "Colma",
+        "coordinates": [
+            "37.684638",
+            "-122.466233"
+        ]
+    },
+    "CONC": {
+        "name": "Concord",
+        "map-name": "Concord",
+        "nickname": "Concord",
+        "coordinates": [
+            "37.973737",
+            "-122.029095"
+        ]
+    },
+    "DALY": {
+        "name": "Daly City",
+        "map-name": "Daly City",
+        "nickname": "Daly City",
+        "coordinates": [
+            "37.706121",
+            "-122.469081"
+        ]
+    },
+    "DBRK": {
+        "name": "Downtown Berkeley",
+        "map-name": "Downtown Berkeley",
+        "nickname": "Dwtn. Berk",
+        "coordinates": [
+            "37.870104",
+            "-122.268133"
+        ]
+    },
+    "DUBL": {
+        "name": "Dublin/Pleasanton",
+        "map-name": "Dublin/Pleasanton",
+        "nickname": "Dublin/Plstn.",
+        "coordinates": [
+            "37.701687",
+            "-121.899179"
+        ]
+    },
+    "DELN": {
+        "name": "El Cerrito del Norte",
+        "map-name": "El Cerrito del Norte",
+        "nickname": "EC del Norte",
+        "coordinates": [
+            "37.925086",
+            "-122.316794"
+        ]
+    },
+    "PLZA": {
+        "name": "El Cerrito Plaza",
+        "map-name": "El Cerrito Plaza",
+        "nickname": "EC Plaza",
+        "coordinates": [
+            "37.902632",
+            "-122.298904"
+        ]
+    },
+    "EMBR": {
+        "name": "Embarcadero",
+        "map-name": "Embarcadero",
+        "nickname": "Embarcadero",
+        "coordinates": [
+            "37.792874",
+            "-122.397020"
+        ]
+    },
+    "FRMT": {
+        "name": "Fremont",
+        "map-name": "Fremont",
+        "nickname": "Fremont",
+        "coordinates": [
+            "37.557465",
+            "-121.976608"
+        ]
+    },
+    "FTVL": {
+        "name": "Fruitvale",
+        "map-name": "Fruitvale",
+        "nickname": "Fruitvale",
+        "coordinates": [
+            "37.774836",
+            "-122.224175"
+        ]
+    },
+    "GLEN": {
+        "name": "Glen Park",
+        "map-name": "Glen Park",
+        "nickname": "Glen Park",
+        "coordinates": [
+            "37.733064",
+            "-122.433817"
+        ]
+    },
+    "HAYW": {
+        "name": "Hayward",
+        "map-name": "Hayward",
+        "nickname": "Hayward",
+        "coordinates": [
+            "37.669723",
+            "-122.087018"
+        ]
+    },
+    "LAFY": {
+        "name": "Lafayette",
+        "map-name": "Lafayette",
+        "nickname": "Lafayette",
+        "coordinates": [
+            "37.893176",
+            "-122.124630"
+        ]
+    },
+    "LAKE": {
+        "name": "Lake Merritt",
+        "map-name": "Lake Merritt",
+        "nickname": "Lake Merritt",
+        "coordinates": [
+            "37.797027",
+            "-122.265180"
+        ]
+    },
+    "MCAR": {
+        "name": "MacArthur",
+        "map-name": "MacArthur",
+        "nickname": "MacArthur",
+        "coordinates": [
+            "37.829065",
+            "-122.267040"
+        ]
+    },
+    "MLBR": {
+        "name": "Millbrae",
+        "map-name": "Millbrae",
+        "nickname": "Millbrae",
+        "coordinates": [
+            "37.600271",
+            "-122.386702"
+        ]
+    },
+    "MONT": {
+        "name": "Montgomery St.",
+        "map-name": "Montgomery St.",
+        "nickname": "Montgomery",
+        "coordinates": [
+            "37.789405",
+            "-122.401066"
+        ]
+    },
+    "NBRK": {
+        "name": "North Berkeley",
+        "map-name": "North Berkeley",
+        "nickname": "N Berk",
+        "coordinates": [
+            "37.873967",
+            "-122.283440"
+        ]
+    },
+    "NCON": {
+        "name": "North Concord/Martinez",
+        "map-name": "North Concord/Martinez",
+        "nickname": "N Concord/Martinez",
+        "coordinates": [
+            "38.003193",
+            "-122.024653"
+        ]
+    },
+    "OAKL": {
+        "name": "Oakland International Airport",
+        "map-name": "Oakland International Airport",
+        "nickname": "OAK Airport",
+        "coordinates": [
+            "37.713238",
+            "-122.212191"
+        ]
+    },
+    "ORIN": {
+        "name": "Orinda",
+        "map-name": "Orinda",
+        "nickname": "Orinda",
+        "coordinates": [
+            "37.878361",
+            "-122.183791"
+        ]
+    },
+    "PITT": {
+        "name": "Pittsburg/Bay Point",
+        "map-name": "Pittsburg/Bay Point",
+        "nickname": "Ptsbg./Bay Point",
+        "coordinates": [
+            "38.018914",
+            "-121.945154"
+        ]
+    },
+    "PCTR": {
+        "name": "Pittsburg Center",
+        "map-name": "Pittsburg Center",
+        "nickname": "Ptsbg. Center",
+        "coordinates": [
+            "38.016941",
+            "-121.889457"
+        ]
+    },
+    "PHIL": {
+        "name": "Pleasant Hill/Contra Costa Centre",
+        "map-name": "Pleasant Hill/Contra Costa Centre",
+        "nickname": "Pleasant Hill",
+        "coordinates": [
+            "37.928468",
+            "-122.056012"
+        ]
+    },
+    "POWL": {
+        "name": "Powell St.",
+        "map-name": "Powell St.",
+        "nickname": "Powell",
+        "coordinates": [
+            "37.784471",
+            "-122.407974"
+        ]
+    },
+    "RICH": {
+        "name": "Richmond",
+        "map-name": "Richmond",
+        "nickname": "Richmond",
+        "coordinates": [
+            "37.936853",
+            "-122.353099"
+        ]
+    },
+    "ROCK": {
+        "name": "Rockridge",
+        "map-name": "Rockridge",
+        "nickname": "Rockridge",
+        "coordinates": [
+            "37.844702",
+            "-122.251371"
+        ]
+    },
+    "SBRN": {
+        "name": "San Bruno",
+        "map-name": "San Bruno",
+        "nickname": "San Bruno",
+        "coordinates": [
+            "37.637761",
+            "-122.416287"
+        ]
+    },
+    "SFIA": {
+        "name": "San Francisco International Airport",
+        "map-name": "San Francisco International Airport",
+        "nickname": "SFO Airport",
+        "coordinates": [
+            "37.615966",
+            "-122.392409"
+        ]
+    },
+    "SANL": {
+        "name": "San Leandro",
+        "map-name": "San Leandro",
+        "nickname": "San Leandro",
+        "coordinates": [
+            "37.721947",
+            "-122.160844"
+        ]
+    },
+    "SHAY": {
+        "name": "South Hayward",
+        "map-name": "South Hayward",
+        "nickname": "S Hayward",
+        "coordinates": [
+            "37.634375",
+            "-122.057189"
+        ]
+    },
+    "SSAN": {
+        "name": "South San Francisco",
+        "map-name": "South San Francisco",
+        "nickname": "South SF",
+        "coordinates": [
+            "37.664245",
+            "-122.443960"
+        ]
+    },
+    "UCTY": {
+        "name": "Union City",
+        "map-name": "Union City",
+        "nickname": "Union City",
+        "coordinates": [
+            "37.590630",
+            "-122.017388"
+        ]
+    },
+    "WCRK": {
+        "name": "Walnut Creek",
+        "map-name": "Walnut Creek",
+        "nickname": "Walnut Creek",
+        "coordinates": [
+            "37.905522",
+            "-122.067527"
+        ]
+    },
+    "WARM": {
+        "name": "Warm Springs/South Fremont",
+        "map-name": "Warm Springs/South Fremont",
+        "nickname": "Warm Springs",
+        "coordinates": [
+            "37.502171",
+            "-121.939313"
+        ]
+    },
+    "WDUB": {
+        "name": "West Dublin/Pleasanton",
+        "map-name": "West Dublin/Pleasanton",
+        "nickname": "W Dublin/Plstn.",
+        "coordinates": [
+            "37.699756",
+            "-121.928240"
+        ]
+    },
+    "WOAK": {
+        "name": "West Oakland",
+        "map-name": "West Oakland",
+        "nickname": "W Oakland",
+        "coordinates": [
+            "37.804872",
+            "-122.295140"
+        ]
+    }
+}
