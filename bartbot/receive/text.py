fa1970c71a024bcd3b0ef20cc64ad3a2786750b3
Anthony Ho <anwyho@gmail.com>
2018-11-21 14:00:30 -0800

    Moved Wit logic out of Text class

--- bartbot/receive/text.py
+++ bartbot/receive/text.py
@@ -4,14 +4,7 @@ import logging
 from typing import (Dict, Optional)
 
 from bartbot.receive.message import (
-    Message, MessageParsingError, ParamType, safe_parse)
-from bartbot.utils.requests import (get)
-from bartbot.utils.urls import (MESSAGES_API, WIT_HEADER,
-                                WIT_MESSAGE_API)
-
-
-class WitParsingError(Exception):
-    pass
+    Message, MessageParsingError, safe_parse)
 
 
 class Text(Message):
@@ -24,29 +17,3 @@ class Text(Message):
         self.text: str = message['text']
         self.quickReply: Optional[str] = message.get(
             'quick_reply', {}).get('payload')
-        self._witEntities: Optional[dict] = None
-
-    @property
-    def entities(self) -> Optional[dict]:
-        """Only parses entities once based off of self.text"""
-        # TODO: Return default response if len(self.text) > 280
-        if self._witEntities is None:
-            data: dict = {
-                'q': self.text[:280],  # Wit has a 280 char limit
-                'n': 4,  # Get 4 best entities
-                'verbose': True,
-                'context': json.dumps({'session_id': self.senderId})}
-
-            ok, witResp = get(WIT_MESSAGE_API, params=data, headers=WIT_HEADER)
-            if ok:
-                logging.info("Successfully called Wit API")
-                logging.debug(f"Wit entities: {json.dumps(witResp,indent=2)}")
-                if 'entities' in witResp:
-                    self._witEntities = witResp['entities']
-                else:
-                    logging.warning("Failed to retrieve Wit entities")
-                    raise WitParsingError(
-                        "Unexpected structure from Wit response.")
-            else:
-                raise WitParsingError("Failed to retrieve Wit entities")
-        return self._witEntities
